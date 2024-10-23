eba25d0a2e1a239daf6a20879a5aca56cc1ec1ba
gioelx <163528341+gioelx@users.noreply.github.com>
2024-10-23 11:42:04 +0200

    Add files via upload

---
+++ php-8/Form/get-post/simple-form-get.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> Form </title>
+</head>
+
+<body>
+
+<form action="welcome-get.php" method="GET"> 
+
+Nome: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+
+<input type="submit">
+
+
+
+
+
+</form>
+
+
+
+<?php
+
+
+
+?>
+
+
+</body>
+
+</html>
---
+++ php-8/Form/get-post/welcome-get.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> Form </title>
+</head>
+
+<body>
+
+
+Benvenuto <?php echo $_GET["name"];     ?><br>
+
+Il tuo indirizzo e-mail è: <?php echo $_GET["email"];     ?>
+
+
+
+
+
+</form>
+
+
+
+<?php
+
+
+
+?>
+
+
+</body>
+
+</html>
+
+
+<!--  l'informazione spedita con un form GET è visibile a tutti, tutte le variabili e i loro valori sono nell'URL -->
